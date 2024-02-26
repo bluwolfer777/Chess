0edd40002f7637d7c1444fc4ba7c4489cf841354
bluwolfer777 <leonleonleonr@gmail.com>
2024-02-26 22:13:03 +0100

    piece class updated

--- Client/Models/Pieces/Piece.cs
+++ Client/Models/Pieces/Piece.cs
@@ -8,5 +8,95 @@ namespace Client.Models
 {
     internal class Piece
     {
+        public bool Mooved { get; set; }
+
+        private int numberPos;
+
+        public int NumberPos
+        {
+            get { return numberPos; }
+            set { numberPos = value; }
+        }
+
+        private int letterPos;
+
+        public int LetterPos
+        {
+            get { return letterPos; }
+            set { letterPos = value; }
+        }
+
+        // true for white false for black
+        private bool color;
+
+        public bool Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public Piece(bool color, string type, int number)
+        {
+            Mooved = false;
+            Color = color;
+            if (color && type != "pawn")
+            {
+                NumberPos = 1;
+            }
+            else if (color && type == "pawn")
+            {
+                NumberPos = 2;
+            }
+            else if (!color && type != "pawn")
+            {
+                NumberPos = 8;
+            }
+            else if (!color && type == "pawn")
+            {
+                NumberPos = 7;
+            }
+
+            switch (type)
+            {
+                case "king":
+                    LetterPos = 5; break;
+                case "queen":
+                    LetterPos = 4; break;
+                case "bishop":
+                    if (number == 1)
+                    {
+                        LetterPos = 3;
+                    }
+                    else
+                    {
+                        LetterPos = 6;
+                    }
+                    break;
+                case "knight":
+                    if (number == 1)
+                    {
+                        LetterPos = 2;
+                    }
+                    else
+                    {
+                        LetterPos = 7;
+                    }
+                    break;
+                case "rook":
+                    if (number == 1)
+                    {
+                        LetterPos = 1;
+                    }
+                    else
+                    {
+                        LetterPos = 8;
+                    }
+                    break;
+                case "pawn":
+                    LetterPos = number; break;
+            }
+        }
+
+        public void moveTo(int x , int y) { }
     }
 }
